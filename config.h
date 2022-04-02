ed7a8dd99babb4668691b3757d0df1203f1f1ba3
danalexandru <danb.alexandru@gmail.com>
2022-04-02 19:09:09 +0300

    add: Changed the font to Ubuntu Mono

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=15:stype=Bold:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono:pixelsize=15:stype=Bold:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
